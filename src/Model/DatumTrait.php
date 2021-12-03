5063804b333fd1ca614fc2ab5528928bbd02d454
ali-kemal.yalama@iubh.de <28_?pL9zb!/99>
2021-12-03 21:04:43 +0100

    Added DatumTrait

---
+++ src/Model/DatumTrait.php
@@ -0,0 +1,96 @@
+<?php
+namespace App\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait DatumTrait {
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $datumErstellt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    protected $datumGeschlossen;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $datumLetzteAenderung;
+
+    /**
+     * @return mixed
+     */
+    public function getDatumErstellt()
+    {
+        return $this->datumErstellt;
+    }
+
+    /**
+     * @param mixed $datumErstellt
+     */
+    public function setDatumErstellt($datumErstellt): void
+    {
+        $this->datumErstellt = $datumErstellt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDatumGeschlossen()
+    {
+        return $this->datumGeschlossen;
+    }
+
+    /**
+     * @param mixed $datumGeschlossen
+     */
+    public function setDatumGeschlossen($datumGeschlossen): void
+    {
+        $this->datumGeschlossen = $datumGeschlossen;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDatumLetzteAenderung()
+    {
+        return $this->datumLetzteAenderung;
+    }
+
+    /**
+     * @param mixed $datumLetzteAenderung
+     */
+    public function setDatumLetzteAenderung($datumLetzteAenderung): void
+    {
+        $this->datumLetzteAenderung = $datumLetzteAenderung;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEinreicher()
+    {
+        return $this->einreicher;
+    }
+
+    /**
+     * @param mixed $einreicher
+     */
+    public function setEinreicher($einreicher): void
+    {
+        $this->einreicher = $einreicher;
+    }
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="eingereichteFehler")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $einreicher;
+
+    public function aktualisiereDatum($date) {
+        $this->datumLetzteAenderung = $date;
+    }
+}
