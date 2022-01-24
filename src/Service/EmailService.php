5392b11de51ac39110f2f82c06a012344fe2698f
karim.saad@iubh.de <karim.saad@iubh.de>
2022-01-25 00:26:04 +0100

    added DSN to ./env.local && added src/Service/EmailService.php && added service to services.yaml

---
+++ src/Service/EmailService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Service;
+
+/**
+ * Provides the System with Sending E-Mail possibilities
+ *
+ * @author karim.saad (karim.saad@iubh.de)
+ * @date 24.01.2022
+ * 
+ * you need to insert MAILER_DSN=smtp://user:pass@smtp.example.com:port in .env.local
+ */
+class EmailService
+{
+    public function __construct ()
+    {
+        
+    }
+}
